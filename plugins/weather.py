80e55a802f2c7de7cd24fe8703808fa529678d03
Michael <m-krjukov@ya.ru>
2017-06-15 03:12:05 +0300

    development of verison 6.0 Отсортированы модели (разбросаны по соответсвующим файлам плагинов); новый плагин-рассылка; теперь плагин погоды рассылает сообщения о погоде всем подписавшимся пользователям;расширено README; теперь у  stopper можно менять время;

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,18 +1,27 @@
-import time
-
 import aiohttp
-import hues
 
+from database import *
 from plugin_system import Plugin
 from utils import schedule_coroutine
 
 plugin = Plugin("Погода",
-                usage="погода - погода")
+                usage=["погода - погода",
+                       "погода подписаться - получать прогноз погоды по утрам",
+                       "погода отписаться - не получать прогноз погода по утрам"])
+
+
+class Weather(BaseModel):
+    uid = peewee.BigIntegerField(unique=True)
+
+    date = peewee.DateTimeField(default=datetime.datetime.now())
+
+Weather.create_table(True)
+
 
 # сервис для определения погоды: http://openweathermap.org/api
 # введите свой ключ, если будете использовать!
 code = "fe198ba65970ed3877578f728f33e0f9"
-default_city = "Москва"
+hour = 8
 
 
 text_to_days = {"завтра": 1, "послезавтра": 2, "через день": 2, "через 1 день": 2,
@@ -28,6 +37,38 @@ async def init(vk):
     plugin.temp_data["weather"] = {}
 
     schedule_coroutine(clear_cache())
+    schedule_coroutine(morning_weather(vk))
+
+
+@plugin.schedule(0)
+async def morning_weather(stopper, vk):
+    n = datetime.datetime.now()
+    s = datetime.datetime(year=n.year, month=n.month, day=n.day, hour=hour) - n
+
+    if 0 > s.total_seconds() > -60:
+        usrs = await db.execute(Weather.select())
+
+        for u in usrs:
+            params = {
+                'user_ids': u.uid,
+                'fields': 'city'
+            }
+            data = await vk.method('users.get', params)
+
+            try:
+                city = data[0]['city']['title']
+            except KeyError:
+                continue
+
+            values = {"user_id": u.uid, "message": await get_weather(city, 0)}
+
+            await vk.method("messages.send", values)
+
+    if s.total_seconds() > 0:
+        stopper.sleep = int(s.total_seconds()) + 2
+
+    else:
+        stopper.sleep = 24 * 60 * 60 + int(s.total_seconds()) + 2
 
 
 @plugin.schedule(10800)  # 3 часа
@@ -35,9 +76,25 @@ async def clear_cache(stopper):
     plugin.temp_data["weather"] = {}
 
 
+@plugin.on_command('погода подписаться')
+async def weather(msg, args):
+    await db.get_or_create(Weather, uid=msg.user_id)
+
+    return await msg.answer("Готово!")
+
+
+@plugin.on_command('погода отписаться')
+async def weather(msg, args):
+    me = await db.execute(Weather.delete().where(Weather.uid == msg.user_id))
+
+    if me:
+        return await msg.answer("Готово!")
+
+    return await msg.answer("Вы не подписаны!")
+
+
 @plugin.on_command('погода')
 async def weather(msg, args):
-    city = default_city
     days = 0
 
     if args:
@@ -53,9 +110,24 @@ async def weather(msg, args):
 
         if possible_city:
             city = possible_city
+    else:
+        params = {
+            'user_ids': msg.user_id,
+            'fields': 'city'
+        }
 
+        data = await msg.vk.method('users.get', params)
+
+        try:
+            city = data[0]['city']['title']
+        except KeyError:
+            return await msg.answer('У вас не указан город в профиле')
+
+    return await msg.answer(await get_weather(city, days))
+
+async def get_weather(city, days):
     if f"{city}{days}" in plugin.temp_data["weather"]:
-        return await msg.answer(plugin.temp_data["weather"][f"{city}{days}"])
+        return plugin.temp_data["weather"][f"{city}{days}"]
 
     if days == 0:
         url = f"http://api.openweathermap.org/data/2.5/weather?APPID={code}&lang=ru&q={city}"
@@ -67,7 +139,7 @@ async def weather(msg, args):
             response = await resp.json()
 
             if "cod" in response and response["cod"] == '404':
-                return await msg.answer("Город не найден!")
+                return "Город не найден!"
 
             if days != 0:
                 answer = f"{city}. Погода.\n\n"
@@ -90,7 +162,8 @@ async def weather(msg, args):
 
                 plugin.temp_data["weather"][f"{city}{days}"] = answer
 
-                return await msg.answer(answer)
+                return answer
+
             else:
                 result = response
 
@@ -109,4 +182,4 @@ async def weather(msg, args):
 
                 plugin.temp_data["weather"][f"{city}{days}"] = answer
 
-                return await msg.answer(answer)
+                return answer
